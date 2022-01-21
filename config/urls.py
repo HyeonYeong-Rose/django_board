2ea5b8c4ab9eb07c6a8bc1568cccf9d5327deff4
HyeonYoeng-Rose <seohy0263@gmail.com>
2022-01-21 15:50:06 +0900

    login logout

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from board import views
 # from board import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('board/', include('board.urls')),
     path('member/',include('member.urls')),
+    path('', views.index, name='index'),
 ]
--- member/urls.py
+++ member/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
         auth_views.LoginView.as_view(template_name='member/login.html'),
         name='login'
     ),
+    path('logout/',
+        auth_views.LogoutView.as_view(),
+        name='logout'
+    ),
 ]
