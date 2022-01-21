4eb587809cc08a160e679625726641f9ec3e1fdf
HyeonYoeng-Rose <seohy0263@gmail.com>
2022-01-21 15:21:46 +0900

    login

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'member', #member.apps.MemberConfig
     'board.apps.BoardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
