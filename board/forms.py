ca22bddf37166b3dc55c8de5eeb479aaaa7ee3be
HyeonYoeng-Rose <seohy0263@gmail.com>
2022-01-21 13:21:23 +0900

    paging completed

--- board/forms.py
+++ board/forms.py
@@ -1,8 +1,16 @@
-from socket import fromshare
+from socket import formshare
 from django import forms
-from board.models import Question
+from board.models import Question, Answer
 
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
         fields = ['subject', 'content']
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+        labels = {
+            'content':'답변내용',
+        }
--- board/views.py
+++ board/views.py
@@ -2,13 +2,20 @@ from multiprocessing import context
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question,Answer
-from .forms import QuestionForm
+from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
+
 # from django.http import HttpResponse
 # Create your views here.
 def index(request):
-
+    page = request.GET.get('page','1') #페이지 입력 파라미터
+    #조회
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+    #페이징 처리
+    paginator = Paginator(question_list, 10) #페이지 당 10개씩
+    page_obj = paginator.get_page(page)
+
+    context = {'question_list': page_obj}
     return render(request, 'board/show.html', context)
 
 def detail(request, question_id):
@@ -34,4 +41,12 @@ def question_create(request):
         form = QuestionForm()
     context = {'form': form}
     return render(request, 'board/q_form.html', context)
-
+    
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer=form.save(commit=False)
+            answer.cretate_date=timezone.now()
+            answer.question = question
